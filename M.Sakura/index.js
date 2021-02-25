46449930cdd007ca825eb86a1c0e4b73b7e6a170
SangLord <79593966+SangLord@users.noreply.github.com>
2021-02-25 18:58:05 +0700

    Add files via upload

---
+++ M.Sakura/index.js
@@ -0,0 +1,11 @@
+APA LIAT LIAT PENGEN UBAH INI JUGA??
+NGOTAK KOMTOL
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+THANKS TO
+-SIZUKA BOT <ow sc>
+-Mr.A43G <dev>
+-LORD ABDUR <sensei>
+-SANG LORD- <dev>
+-alpin ofc <persediaan apikey vhter>
+-ALL DEV BOT LAENNYA
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
